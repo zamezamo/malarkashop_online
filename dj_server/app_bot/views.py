8fcc9f867845cb8ddc85b835c3555cee04c3a6f7
zamezamo <zamzame53@gmail.com>
2024-08-29 16:47:52 +0300

    update

--- dj_server/app_bot/views.py
+++ dj_server/app_bot/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from dj_server.settings import TITLE
 
 # Create your views here.
+
+context = {'title': TITLE}
+def index(request):
+    return render(request, 'app_bot/index.html', context)
--- dj_server/dj_server/settings.py
+++ dj_server/dj_server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app_bot.apps.AppBotConfig'
 ]
 
 MIDDLEWARE = [
@@ -117,7 +120,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TITLE = 'AutoCustomersStore'
