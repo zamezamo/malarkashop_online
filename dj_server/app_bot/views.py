97d3554369e0157bdd879d8583ddae1981c4d188
zamezamo <zamzame53@gmail.com>
2024-09-02 16:29:50 +0300

    async

--- dj_server/app_bot/views.py
+++ dj_server/app_bot/views.py
@@ -4,5 +4,5 @@ from dj_server.settings import TITLE
 # Create your views here.
 
 context = {'title': TITLE}
-def index(request):
+async def index(request):
     return render(request, 'app_bot/index.html', context)
