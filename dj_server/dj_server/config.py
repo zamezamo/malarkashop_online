52c0113f02f00c271f3aecdfea313a64aaca8571
zamezamo <zamzame53@gmail.com>
2024-09-21 12:36:12 +0300

    config for bot

---
+++ dj_server/dj_server/config.py
@@ -0,0 +1,8 @@
+TITLE = 'AutoCustomersStore'
+BOT_LINK="tg://resolve?domain=autocosmeticsstore_bot"
+
+# Define bot configuration constants
+URL = "https://2b32-178-122-9-4.ngrok-free.app"
+ADMIN_CHAT_ID = 542399495 # @zamezamo
+PORT = 8000
+TOKEN = "7000362389:AAFGsZk51Japmkc_U6cXqmHM3IFOPo8eCI0"  # nosec B105
