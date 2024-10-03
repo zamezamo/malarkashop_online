759c8357fc84f43c02a41c32fb0ad8a5962d2742
zamezamo <zamzame53@gmail.com>
2024-10-03 21:24:31 +0300

    update models and config

--- dj_server/app_bot/models.py
+++ dj_server/app_bot/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 
+from dj_server.config import CATEGORY_CHOICES
+
 # Create your models here.
 
 class Admin(models.Model):
     admin_id = models.BigIntegerField()
+    is_notification_enabled = models.BooleanField()
 
 class User(models.Model):
     user_id = models.BigIntegerField()
 
 class Part(models.Model):
     part_id = models.AutoField(primary_key=True)
-    image = models.FileField(upload_to="settings.MEDIA_ROOT/parts", default="default_part.jpg", name="1")
-    available_count = models.IntegerField(default=0)
     name = models.CharField(max_length=64)
+    category = models.CharField(max_length=16, choices=CATEGORY_CHOICES)
     description = models.TextField(max_length=256)
+    image = models.FileField(upload_to="settings.MEDIA_ROOT/parts", default="default_part.jpg", name="CHAAANGEEE IT")
+    available_count = models.IntegerField(default=0)
 
 class Order(models.Model):
     order_id = models.BigIntegerField(primary_key=True)
--- dj_server/dj_server/config.py
+++ dj_server/dj_server/config.py
@@ -2,7 +2,27 @@ TITLE = 'AutoCustomersStore'
 BOT_LINK="tg://resolve?domain=autocosmeticsstore_bot"
 
 # Define bot configuration constants
-URL = "https://2b32-178-122-9-4.ngrok-free.app"
+URL = "https://83e1-178-124-178-90.ngrok-free.app"
 ADMIN_CHAT_ID = 542399495 # @zamezamo
 PORT = 8000
-TOKEN = "7000362389:AAFGsZk51Japmkc_U6cXqmHM3IFOPo8eCI0"  # nosec B105
+TOKEN = "7000362389:AAFGsZk51Japmkc_U6cXqmHM3IFOPo8eCI0"  # KEEP IT IN SECRET!
+
+START_TEXT = (
+    f"Добро пожаловать в *{TITLE}*!\n"
+    f"Подписывайтесь на наш [канал]({BOT_LINK})!\n"
+    f"\n"
+    f"описание\nописание\nописание\nописание\n"
+    f"\n"
+    f"Для того чтобы перейти в каталог нажмите _кнопку_ ниже\n"
+)
+
+CATEGORY_CHOICES = {
+    "ABRAS_MATHERIALS": "абразивные материалы",
+    "POLISHING_WHEELS": "полировальные круги",
+    "PAINTING_TAPES": "малярные ленты",
+    "PLANES": "рубанки",
+    "POLISHING_PASTES": "полировальные пасты",
+    "SPRAY_GUNS": "краскопульты",
+    "SUPPLIES": "расходные материалы",
+    "OTHER": "другое"
+}
