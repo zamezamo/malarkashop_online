39a5539e45bdc4b82aeae629cb15222575d2a5d1
zamezamo <zamzame53@gmail.com>
2024-10-04 16:13:27 +0300

    register models

--- dj_server/app_bot/admin.py
+++ dj_server/app_bot/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (
+    Admin, User, Part, Order, CompletedOrder
+)
 
 # Register your models here.
+
+admin.site.register(Admin)
+admin.site.register(User)
+admin.site.register(Part)
+admin.site.register(Order)
+admin.site.register(CompletedOrder)
--- dj_server/app_bot/models.py
+++ dj_server/app_bot/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 from dj_server.config import CATEGORY_CHOICES
@@ -6,31 +8,31 @@ from dj_server.config import CATEGORY_CHOICES
 
 class Admin(models.Model):
     admin_id = models.BigIntegerField()
-    is_notification_enabled = models.BooleanField()
+    is_notification_enabled = models.BooleanField(default=True)
 
 class User(models.Model):
     user_id = models.BigIntegerField()
 
 class Part(models.Model):
-    part_id = models.AutoField(primary_key=True)
+    part_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=64)
-    category = models.CharField(max_length=16, choices=CATEGORY_CHOICES)
+    category = models.CharField(max_length=16, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES["OTHER"])
     description = models.TextField(max_length=256)
-    image = models.FileField(upload_to="settings.MEDIA_ROOT/parts", default="default_part.jpg", name="CHAAANGEEE IT")
     available_count = models.IntegerField(default=0)
+    image = models.FileField(upload_to="settings.MEDIA_ROOT/img/parts", default="no_img_part.jpg", name=f"part_{name}_{part_id}")
 
 class Order(models.Model):
-    order_id = models.BigIntegerField(primary_key=True)
-    part_id = models.ForeignKey(Part, on_delete=models.CASCADE)
+    order_id = models.BigAutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    ordered_time = models.DateTimeField()
-    is_accepted = models.BooleanField()
-    accepted_time = models.DateTimeField()
+    parts = models.JSONField(default={})
+    ordered_time = models.DateTimeField(default=datetime.now())
+    is_accepted = models.BooleanField(default=False)
+    accepted_time = models.DateTimeField(default=datetime.now())
 
 class CompletedOrder(models.Model):
     order_id = models.BigIntegerField(primary_key=True)
-    part_id = models.ForeignKey(Part, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    ordered_time = models.DateTimeField()
-    accepted_time = models.DateTimeField()
-    completed_time = models.DateTimeField()
+    user_id = models.BigIntegerField()
+    parts = models.JSONField(default={})
+    ordered_time = models.DateTimeField(default=datetime.now())
+    accepted_time = models.DateTimeField(default=datetime.now())
+    completed_time = models.DateTimeField(default=datetime.now())
