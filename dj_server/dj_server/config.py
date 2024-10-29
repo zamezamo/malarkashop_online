028320063c27e5f32769cbc70b836b845992111c
zamezamo <zamzame53@gmail.com>
2024-10-29 15:16:51 +0300

    update category imgs

--- dj_server/dj_server/config.py
+++ dj_server/dj_server/config.py
@@ -2,7 +2,7 @@ TITLE = 'AutoCustomersStore'
 BOT_LINK="tg://resolve?domain=autocosmeticsstore_bot"
 
 # Define bot configuration constants
-URL = "https://83e1-178-124-178-90.ngrok-free.app"
+URL = "https://0524-2a09-bac5-506f-2dc-00-49-1e4.ngrok-free.app"
 ADMIN_CHAT_ID = 542399495 # @zamezamo
 PORT = 8000
 TOKEN = "7000362389:AAFGsZk51Japmkc_U6cXqmHM3IFOPo8eCI0"  # KEEP IT IN SECRET!
@@ -20,20 +20,13 @@ CHOOSE_CATEGORY_TEXT = (
     f"\n_выберите категорию товара ниже:_"
 )
 
-def CATEGORY_CARDS_TEXT(category: str, name: str, available_count: int, description: str):
-    return (
-        f"[**{CATEGORY_CHOICES[category]}**]\n"
-        f"\n\n"
-        f""
-    )
-
 CATEGORY_CHOICES = {
-    "ABRAS_MATHERIALS": "абразивные материалы",
-    "POLISHING_WHEELS": "полировальные круги",
-    "PAINTING_TAPES": "малярные ленты",
+    "ABRSMATS": "абразивные материалы",
+    "POLWHEEL": "полировальные круги",
+    "PNTTAPES": "малярные ленты",
     "PLANES": "рубанки",
-    "POLISHING_PASTES": "полировальные пасты",
-    "SPRAY_GUNS": "краскопульты",
+    "POLPASTS": "полировальные пасты",
+    "SPRAYGUN": "краскопульты",
     "SUPPLIES": "расходные материалы",
     "OTHER": "другое"
 }
